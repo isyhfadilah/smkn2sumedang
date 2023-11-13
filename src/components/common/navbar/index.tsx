862edefa7bc4ef44b1abaaf4b8cbe1985dc4667d
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-11-14 03:02:06 +0700

    fix: padding in navbar component

--- src/components/common/navbar/index.tsx
+++ src/components/common/navbar/index.tsx
@@ -7,7 +7,7 @@ import { contact } from '@/data/contact'
 export const Navbar = () => {
   return (
     <nav className="w-full fixed z-10 bg-white">
-      <div className="bg-primary w-full py-3">
+      <div className="bg-primary w-full py-2">
         <Container className="flex justify-end">
           <ul className="text-white text-sm flex space-x-8">
             {contact.map((item, index) => (
