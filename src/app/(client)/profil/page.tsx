30fba3c52d3ea87f477d6a6972882b6a510e75ec
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-11-15 11:27:44 +0700

    feat: add sejarah section in profil page

--- src/app/(client)/profil/page.tsx
+++ src/app/(client)/profil/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from '@/components/common/hero'
+import { ProfilSejarah } from '@/components/common/sections/profil/sejarah'
 
 export default function Profil() {
   return (
     <main>
       <Hero image="/images/landing/hero.png" title="Profil SMKN 2 Sumedang" />
+      <ProfilSejarah />
     </main>
   )
 }
---
+++ src/components/common/sections/profil/index.tsx
@@ -0,0 +1,1 @@
+export { ProfilSejarah } from './sejarah'
