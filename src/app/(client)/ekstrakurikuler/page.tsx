e0cc7c1714c14453889aeb652efe3353eed7d981
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-11-15 15:39:23 +0700

    feat: add ekstrakurikuler section

--- src/app/(client)/ekstrakurikuler/page.tsx
+++ src/app/(client)/ekstrakurikuler/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from '@/components/common/hero'
+import { EkstrakurikulerSection } from '@/components/common/sections/ekstrakurikuler'
 
 export default function Ekstrakurikuler() {
   return (
     <main>
       <Hero image="/images/landing/hero.png" title="Ekstrakurikuler" />
+      <EkstrakurikulerSection />
     </main>
   )
 }
---
+++ src/components/common/sections/ekstrakurikuler/ekstrakurikuler.tsx
@@ -0,0 +1,12 @@
+import Container from '../../layouts/Container'
+import { Title } from '@/components/ui/title'
+
+export const EkstrakurikulerSection = () => {
+  return (
+    <section className="my-16">
+      <Container>
+        <Title title="Ekstrakurikuler" subtitle="Tingkatkan Berbagai Minat dan Bakat" className="text-center" />
+      </Container>
+    </section>
+  )
+}
