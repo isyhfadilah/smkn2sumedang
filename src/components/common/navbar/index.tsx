53e6d41dead7e7b3b05f9693573ad3c241405ce2
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-11-25 19:24:18 +0700

    feat(mobile): add menu n transition on navbar component

--- src/components/common/navbar/index.tsx
+++ src/components/common/navbar/index.tsx
@@ -5,14 +5,20 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { menu } from '@/data/menu'
 import { contact } from '@/data/contact'
-import { usePathname } from 'next/navigation'
 import { cn } from '@/lib/utils'
+
+import { useState } from 'react'
+import { usePathname } from 'next/navigation'
+
+import { IoClose, IoMenu } from 'react-icons/io5'
+import { Transition, Menu } from '@headlessui/react'
 
 export const Navbar = () => {
   const pathname = usePathname()
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <nav className="w-full fixed z-10 bg-white">
-      <div className="bg-primary w-full py-2">
+      <div className="hidden lg:block bg-primary w-full py-2">
         <Container className="flex justify-end">
           <ul className="text-white text-sm flex space-x-8">
             {contact.map((item, index) => (
@@ -35,13 +41,49 @@ export const Navbar = () => {
             <li key={index}>
               <Link
                 href={item.link}
-                className={cn('text-primary', pathname === item.link ? 'font-bold text-[#065986]' : 'font-medium')}>
+                className={cn(
+                  'hidden lg:block text-primary',
+                  pathname === item.link ? 'font-bold text-[#065986]' : 'font-medium'
+                )}>
                 {item.name}
               </Link>
             </li>
           ))}
         </ul>
+        {isOpen ? (
+          <IoClose onClick={() => setIsOpen(!isOpen)} size={35} className="lg:hidden text-primary cursor-pointer" />
+        ) : (
+          <IoMenu onClick={() => setIsOpen(!isOpen)} size={35} className="lg:hidden text-primary cursor-pointer" />
+        )}
       </Container>
+
+      <Transition
+        show={isOpen}
+        enter="transition-all duration-500"
+        enterFrom="max-h-0 opacity-0"
+        leave="transition-all duration-1000"
+        leaveFrom="max-h-screen opacity-100"
+        leaveTo="max-h-0 opacity-0"
+        className="bg-white px-5 pt-5 h-screen lg:hidden">
+        <Menu>
+          <Container>
+            <ul className="flex flex-col space-y-10">
+              {menu.map((item, index) => (
+                <li key={index}>
+                  <Link
+                    href={item.link}
+                    className={cn(
+                      'text-primary text-lg',
+                      pathname === item.link ? 'font-bold text-[#065986]' : 'font-medium'
+                    )}>
+                    {item.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Container>
+        </Menu>
+      </Transition>
     </nav>
   )
 }
