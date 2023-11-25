2b7e7dce854124b73bfc74219c804af1961168a6
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-11-25 21:08:31 +0700

    fix: grid cols in mitra section

--- src/components/common/sections/landing/mitra.tsx
+++ src/components/common/sections/landing/mitra.tsx
@@ -8,7 +8,7 @@ export const LandingMitra = () => {
       <Container className="flex flex-col justify-between items-center">
         <p className="text-sm text-dark">Mitra</p>
         <h1 className="text-3xl font-medium">Mitra Industri</h1>
-        <div className="grid grid-cols-4 gap-16 items-center mt-6 w-full">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-16 items-center mt-10 w-full">
           {mitra.map((item, index) => (
             <Image key={index} src={item.icon} alt={item.name} height={58} width={200} />
           ))}
