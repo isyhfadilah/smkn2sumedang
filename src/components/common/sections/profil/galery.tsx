ec76cd968c2f3e62c9ab1a0f632f6e39b7c5aa2f
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-11-25 18:37:35 +0700

    feat: add fasilitas image in profil page

--- src/components/common/sections/profil/galery.tsx
+++ src/components/common/sections/profil/galery.tsx
@@ -1,11 +1,17 @@
 import Container from '../../layouts/Container'
 import { Title } from '@/components/ui/title'
+import fasilitas from '@/data/fasilitas.json'
 
 export const ProfilGalery = () => {
   return (
     <section className="my-16">
-      <Container>
+      <Container className="w-full">
         <Title title="Galeri" subtitle="Fasilitas" className="text-center" />
+        <div className="flex flex-wrap gap-6 items-center justify-between mt-10">
+          {fasilitas.map((item, index) => (
+            <img key={index} src={item} alt={item} />
+          ))}
+        </div>
       </Container>
     </section>
   )
