69ece2a6d110c441e0593f4e606e5f26fd038886
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-11-26 17:35:50 +0700

    fix: width n font size in hero component

--- src/components/common/hero/index.tsx
+++ src/components/common/hero/index.tsx
@@ -12,8 +12,8 @@ export const Hero: React.FC<Props> = ({ title, image, description }) => {
   }
 
   return (
-    <section style={backgroundImage} className="pt-56 pb-26 bg-cover bg-no-repeat bg-center text-white">
-      <h1 className="font-semibold text-3xl text-center">{title}</h1>
+    <section style={backgroundImage} className="pt-56 pb-36 lg:pb-26 bg-cover bg-no-repeat bg-center text-white w-full">
+      <h1 className="font-semibold text-2xl lg:text-3xl text-center">{title}</h1>
       <p className="text-center">{description}</p>
     </section>
   )
