f3a883e899de4b62347b22dd45c430679da8cc71
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-11-26 18:07:06 +0700

    fix: layout in struktural n visi section

--- src/components/common/hero/index.tsx
+++ src/components/common/hero/index.tsx
@@ -12,7 +12,7 @@ export const Hero: React.FC<Props> = ({ title, image, description }) => {
   }
 
   return (
-    <section style={backgroundImage} className="pt-56 pb-36 lg:pb-26 bg-cover bg-no-repeat bg-center text-white w-full">
+    <section style={backgroundImage} className="pt-56 pb-36 lg:pb-26 bg-cover bg-no-repeat bg-center text-white">
       <h1 className="font-semibold text-2xl lg:text-3xl text-center">{title}</h1>
       <p className="text-center">{description}</p>
     </section>
--- src/components/common/sections/profil/struktural.tsx
+++ src/components/common/sections/profil/struktural.tsx
@@ -8,7 +8,7 @@ export const ProfilStruktural = () => {
     <section className="my-16">
       <Container className="flex flex-col items-center">
         <Title title="Struktural" subtitle="Posisi Struktural" className="text-center" />
-        <div className="grid grid-cols-3 gap-6 mt-6">
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
           {struktural.map((item, index) => (
             <CardStruktural key={index} image={item.image} name={item.name} position={item.position} />
           ))}
