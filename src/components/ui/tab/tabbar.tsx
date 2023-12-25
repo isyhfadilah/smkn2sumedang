32871ca30530e4e4d468ebfac544d3f4dee0b766
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-12-25 16:17:16 +0700

    fix: active index in tabbar

--- src/components/ui/tab/tabbar.tsx
+++ src/components/ui/tab/tabbar.tsx
@@ -10,13 +10,15 @@ interface Props {
 
 export const Tabbar = ({ tabs, activeIndex, onTabClick }: Props) => {
   return (
-    <nav className="flex space-x-2xl mb-10">
+    <nav className="flex space-x-2 lg:space-x-5xl mb-10">
       {tabs.map((tab, index) => (
         <div
           key={index}
-          className={`text-lg ${activeIndex === index ? 'font-bold' : 'font-light'}`}
+          className={`${
+            activeIndex === index ? 'text-black font-medium  border-b-2 border-primary' : 'font-light text-black/50'
+          }`}
           onClick={() => onTabClick(index)}>
-          <p className="text-primary cursor-pointer">{tab.label}</p>
+          <p className="lg:text-lg cursor-pointer">{tab.label}</p>
         </div>
       ))}
     </nav>
