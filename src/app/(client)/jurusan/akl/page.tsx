7decb86889cb98222c6cbec96855d4b534b2fb3e
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-12-26 22:43:49 +0700

    feat: add akuntasi page

---
+++ src/app/(client)/jurusan/akl/page.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { Tabbar, TabContent } from '@/components/ui/tab'
+import { CardKurikulum } from '@/components/ui/cards/kurikulum'
+import jurusan from '@/data/akl.json'
+import Container from '@/components/common/layouts/Container'
+import { useState } from 'react'
+import { HeroJurusan } from '@/components/common/sections/hero'
+
+export default function Akl() {
+  const [activeIndex, setActiveIndex] = useState(0)
+
+  const images = [
+    '/images/jurusan/akl/akl-slide-1.png',
+    '/images/jurusan/akl/akl-slide-2.png',
+    '/images/jurusan/akl/akl-slide-3.png'
+  ]
+
+  return (
+    <main>
+      <HeroJurusan
+        title="Akuntansi"
+        description="Akuntansi adalah salah satu bidang studi yang memiliki peran penting dalam kegiatan bisnis."
+        images={images}
+      />
+      <Container className="py-16 lg:flex">
+        <div className="mr-10 w-full">
+          <Tabbar tabs={jurusan} activeIndex={activeIndex} onTabClick={(index) => setActiveIndex(index)} />
+          <TabContent
+            title={jurusan[activeIndex].title}
+            description={jurusan[activeIndex]?.detail}
+            subtitle={jurusan[activeIndex]?.subtitle}
+            pointer={jurusan[activeIndex]?.pointer}
+            icon={jurusan[activeIndex]?.icon}
+            image={jurusan[activeIndex]?.image}
+            name={jurusan[activeIndex]?.name}
+            position={jurusan[activeIndex]?.position}
+          />
+        </div>
+        <CardKurikulum />
+      </Container>
+    </main>
+  )
+}
