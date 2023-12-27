476ddefa643f61994109ec54bacec1478bfbe3d6
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2023-12-27 21:51:50 +0700

    fix: data kurikulum all jurusan

--- src/data/kurikulum.ts
+++ src/data/kurikulum.ts
@@ -6,3 +6,36 @@ export const pplg = [
   'Pemrograman Web',
   'Pemrograman Berorientasi Objek'
 ]
+
+export const akl = [
+  'Ekonomi bisnis dan administrasi umum',
+  'Akuntansi perusahaan jasa, dagang dan manufaktur',
+  'Akuntansi lembaga/instansi pemerintah',
+  'Akuntansi keuangan',
+  'Komputer akuntansi',
+  'Perpajakan'
+]
+
+export const mplb = [
+  'Ekonomi dan bisnis',
+  'Pengelolaan administrasi umum',
+  'Komunikasi di tempat kerja',
+  'Pengelolaan kearsipan',
+  'Teknologi Perkantoran',
+  'Pengelolaan rapat/pertemuan',
+  'Pengelolaan keuangan sederhana',
+  'Pengelolaan sumber daya manusia (SDM)',
+  'Pengelolaan sarana dan prasarana',
+  'Pengelolaan humas dan keprotokolan'
+]
+
+export const pm = [
+  'Ekonomi bisnis dan administrasi umum',
+  'Marketing',
+  'Customer service',
+  'Komunikasi Bisnis',
+  'Pengelolaan Bisnis Retail',
+  'Strategi marketing visual merchandising',
+  'Pengemasan dan pendistribusian produk',
+  'Administrasi transaksi'
+]
