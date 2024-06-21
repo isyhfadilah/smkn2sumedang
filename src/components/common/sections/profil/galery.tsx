053a1f32e844693e47a9e42f3942fd2c220b681d
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2024-06-21 10:30:15 +0700

    fix: layouting image on gallery in profil section

--- src/components/common/sections/profil/galery.tsx
+++ src/components/common/sections/profil/galery.tsx
@@ -1,6 +1,7 @@
 import Container from '../../layouts/Container'
 import { Title } from '@/components/ui/title'
 import fasilitas from '@/data/fasilitas.json'
+import Image from 'next/image'
 
 export const ProfilGalery = () => {
   return (
@@ -8,9 +9,18 @@ export const ProfilGalery = () => {
       <Container className="w-full">
         <Title title="Galeri" subtitle="Fasilitas" className="text-center" />
         <div className="flex flex-wrap gap-6 items-center justify-between mt-10">
-          {fasilitas.map((item, index) => (
-            <img key={index} src={item} alt={item} />
-          ))}
+          <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 place-items-center">
+            {fasilitas.map((item, index) => (
+              <Image
+                key={index}
+                src={item}
+                alt={item}
+                height={1000}
+                width={1000}
+                className="w-100 h-50 bg-cover bg-center bg-no-repeat object-cover"
+              />
+            ))}
+          </div>
         </div>
       </Container>
     </section>
